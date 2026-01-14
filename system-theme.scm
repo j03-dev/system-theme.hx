36b69fc6442525e260c10703bb57a61fdb910b89
FITAHIANA Nomeniavo Joe <24nomeniavo@gmail.com>
2026-01-14 20:02:07 +0300

    first working version of watching theme

--- system-theme.scm
+++ system-theme.scm
@@ -1,12 +1,30 @@
 (require "system-theme-hx.scm")
+(require "helix/ext.scm")
 (require (only-in "helix/commands.scm" theme))
+(require-builtin steel/time)
 
 (provide auto-theme)
 
-;;@doc  
-;; Automatically switches between dark and light themes based on system preference
-;; Usage: (auto-theme "gruvbox" "gruvbox-light")
-;;        (auto-theme "catppuccin-mocha" "catppuccin-latte")
-(define (auto-theme dark light)
-  (if (equal? "dark" (detect))(theme dark) (theme light)))
+(define current-theme #f)
 
+(define (apply-theme dark light)
+  (define detected (detect))
+  (unless (equal? detected current-theme)
+    (set! current-theme detected)
+    (if (equal? detected "dark")
+        (theme dark)
+        (theme light))))
+
+(define (auto-theme dark light [interval-ms 2000])
+  ;; Apply once immediately
+  (apply-theme dark light)
+
+  ;; Background polling loop
+  (spawn-native-thread
+   (lambda ()
+     (let loop ()
+       (time/sleep-ms interval-ms)
+       (hx.with-context
+        (lambda ()
+          (apply-theme dark light)))
+       (loop)))))
